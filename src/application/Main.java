e7381bbf1003bd898cb093e036071a9e90538617
Bitwise Star <imalicedev@gmail.com>
2025-03-27 19:48:09 -0300

    feat: added seller class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,16 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(1, "Julio", "bob@gmail.com", new Date(), 2000.0, department);
 
+        System.out.println(seller);
+        System.out.println(department);
     }
 }
